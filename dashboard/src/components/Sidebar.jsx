334dbd2a9e6743521cbc41707edce6913ee6f942
shriyapurwar15 <98403273+shriyapurwar15@users.noreply.github.com>
2024-07-09 17:31:16 +0530

    url change

--- dashboard/src/components/Sidebar.jsx
+++ dashboard/src/components/Sidebar.jsx
@@ -18,7 +18,7 @@ const Sidebar = () => {
 
   const handleLogout = async () => {
     await axios
-      .get("http://localhost:4000/api/v1/user/admin/logout", {
+      .get("https://hospital-management-pbe9.onrender.com/api/v1/user/admin/logout", {
         withCredentials: true,
       })
       .then((res) => {
