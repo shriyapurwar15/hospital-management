cfcb3dac4611a47c79ce54954c41491451cf5c64
DJ <97788700+DJMONSTER0212@users.noreply.github.com>
2024-08-03 20:21:34 +0530

    cookie error debugged test-1

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -9,6 +9,8 @@ export const generateToken = (user, message, statusCode, res) => {
       expires: new Date(
         Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
       ),
+      sameSite: 'None',
+      secure: true,
       httpOnly: true,
     })
     .json({
